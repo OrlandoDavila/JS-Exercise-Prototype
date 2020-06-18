96401a0636949032de6a00bfb89898e2c7b6ecf2
Orlando Davila <odavila0308@gmail.com>
2020-06-18 18:50:42 -0400

    todays project

--- index.js
+++ index.js
@@ -39,9 +39,31 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function(edible){
+  if(this.stomach.length < 10){
+    this.stomach.push(edible);
+  }
+}
+
+Person.prototype.poop = function(){
+  this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+  return `${this.name}, ${this.age}`;
+}
+
+const personOne = new Person('Orlando', 35);
+personOne.eat('tacos');
+console.log(personOne.stomach);
+personOne.poop();
+console.log(personOne.stomach);
 
 /*
   TASK 2
@@ -57,9 +79,22 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
+
+Car.prototype.fill = function(gallons){
+ return this.tank += gallons;
+};
+
+const carNew = new Car('Tucon', 24);
+
+carNew.fill(50);
+console.log(carNew.tank);
+
 
 /*
   TASK 3
@@ -68,18 +103,33 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name,age,favoriteToy) {
+  Person.call(this,name,age);
+  this.favoriteToy=favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
+Baby.prototype.play = function(){
+  return `Playing with ${this.favoriteToy}`
+}
+
+console.log()
 
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Window/ Global Object Binding- If no other rules apply 'this' defaults to window 
+
+
+  2. Implicit Binding- occurs when dot notation is used to invoke a function
+
+
+  3. New Binding- bind() generates a new function when called. its sets the `this` to a value. 
+
+
+  4. Explicit Binding- When .call(), .apply() or .bid() are used.
+
+
 */
 
 
